0851aa9f117b0ac7c4a803a5cfd85c6904bc4bdd
jgreenb2 <jgreenb@mac.com>
2015-07-08 20:54:47 -0400

    loads thumbnails

---
+++ Trax/GPXViewController.swift
@@ -0,0 +1,101 @@
+//
+//  ViewController.swift
+//  Trax
+//
+//  Created by Jeff Greenberg on 7/7/15.
+//  Copyright (c) 2015 Jeff Greenberg. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class GPXViewController: UIViewController, MKMapViewDelegate {
+
+    @IBOutlet weak var mapView: MKMapView! {
+        didSet {
+            mapView.mapType = .Satellite
+            mapView.delegate = self
+        }
+    }
+    
+    var gpxURL: NSURL? {
+        didSet {
+            if let url = gpxURL {
+                clearWayPoints()
+                GPX.parse(url) {
+                    if let gpx = $0 {
+                        self.handleWayPoints(gpx.waypoints)
+                    }
+                }
+            }
+        }
+    }
+    
+    private func clearWayPoints() {
+        if mapView?.annotations != nil {
+            mapView.removeAnnotations(mapView.annotations as! [MKAnnotation])
+        }
+    }
+    
+    private func handleWayPoints(waypoints: [GPX.Waypoint]) {
+        mapView.addAnnotations(waypoints)
+        mapView.showAnnotations(waypoints, animated: true)
+    }
+    
+    struct Constants {
+        static let AnnotationViewReuseIdentifier = "waypoints"
+        static let LeftCalloutFrame = CGRect(x: 0, y: 0, width: 59, height: 59)
+    }
+    
+    func mapView(mapView: MKMapView!, viewForAnnotation annotation: MKAnnotation!) -> MKAnnotationView! {
+        var view = mapView.dequeueReusableAnnotationViewWithIdentifier(Constants.AnnotationViewReuseIdentifier)
+        
+        if view == nil {
+            view = MKPinAnnotationView(annotation: annotation, reuseIdentifier: Constants.AnnotationViewReuseIdentifier)
+            view.canShowCallout = true
+        } else {
+            view.annotation = annotation
+        }
+        
+        view.leftCalloutAccessoryView = nil
+        if let waypoint = annotation as? GPX.Waypoint {
+            if waypoint.thumbnailURL != nil {
+                view.leftCalloutAccessoryView = UIImageView(frame: Constants.LeftCalloutFrame)
+            }
+        }
+        return view
+    }
+    
+    func mapView(mapView: MKMapView!, didSelectAnnotationView view: MKAnnotationView!) {
+        if let waypoint = view.annotation as? GPX.Waypoint {
+            if let thumbnailImageView = view.leftCalloutAccessoryView as? UIImageView {
+                if let imageData = NSData(contentsOfURL: waypoint.thumbnailURL!) { // fix! by making async
+                    if let image = UIImage(data: imageData) {
+                        thumbnailImageView.image = image
+                    }
+                }
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let center = NSNotificationCenter.defaultCenter()
+        let queue = NSOperationQueue.mainQueue()
+        let appDelegate = UIApplication.sharedApplication().delegate
+        
+        center.addObserverForName(GPXURL.Notification, object: appDelegate, queue: queue) { (notification) -> Void in
+            if let url = notification?.userInfo?[GPXURL.Key] as? NSURL {
+                self.gpxURL = url
+            }
+        }
+        
+        gpxURL = NSURL(string: "http://cs193p.stanford.edu/Vacation.gpx")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
+
---
+++ Trax/MKGPX.swift
@@ -0,0 +1,29 @@
+//
+//  MKGPX.swift
+//  Trax
+//
+//  Created by Jeff Greenberg on 7/8/15.
+//  Copyright (c) 2015 Jeff Greenberg. All rights reserved.
+//
+
+import MapKit
+
+extension GPX.Waypoint: MKAnnotation {
+    var coordinate: CLLocationCoordinate2D {
+        return CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+    }
+    var title: String! {return name}
+    var subtitle: String! {return info}
+
+    var  thumbnailURL: NSURL? { return getImageURLofType("thumbnail") }
+    var imageURL: NSURL? { return getImageURLofType("large") }
+        
+    private func getImageURLofType(type: String) -> NSURL? {
+        for link in links {
+            if link.type == type {
+                return link.url
+            }
+        }
+        return nil
+    }
+}
