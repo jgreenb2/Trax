6244e0f66308c4e68945ca53d5b62c849a2b620c
jgreenb2 <jgreenb@mac.com>
2015-07-12 20:46:17 -0400

    embedded view controller for waypoint

---
+++ Trax/SimpleMapViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SimpleMapViewController.swift
+//  Trax
+//
+//  Created by Jeff Greenberg on 7/12/15.
+//  Copyright (c) 2015 Jeff Greenberg. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class SimpleMapViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+
+}
---
+++ Trax/WaypointImageViewController.swift
@@ -0,0 +1,38 @@
+//
+//  WaypointImageViewController.swift
+//  Trax
+//
+//  Created by Jeff Greenberg on 7/12/15.
+//  Copyright (c) 2015 Jeff Greenberg. All rights reserved.
+//
+
+import UIKit
+
+class WaypointImageViewController: ImageViewController {
+
+    var waypoint: GPX.Waypoint? {
+        didSet {
+            imageURL = waypoint?.imageURL
+            title = waypoint?.name
+            updateEmbeddedMap()
+        }
+    }
+    
+    var smvc: SimpleMapViewController?
+    
+    func updateEmbeddedMap() {
+        if let mapView = smvc?.mapView {
+            mapView.mapType = .Hybrid
+            mapView.removeAnnotation(mapView.annotations)
+            mapView.addAnnotation(waypoint)
+            mapView.showAnnotations(mapView.annotations, animated: true)
+        }
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "embed map" {
+            smvc = segue.destinationViewController.contentViewController as? SimpleMapViewController
+            updateEmbeddedMap()
+        }
+    }
+}
